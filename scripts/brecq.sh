098d82423abbbfc9a1db62cf10113616f62ee9da
Areeb Syed <79010410+heisenbug237@users.noreply.github.com>
2022-07-14 12:57:29 +0530

    Update brecq.sh

--- scripts/brecq.sh
+++ scripts/brecq.sh
@@ -1,6 +1,6 @@
-export datasets_dir = /PATH/TO/DATA
-export model = resnet18
-export w_bits = 4
-export a_bits = 8
+export datasets_dir=/PATH/TO/DATA
+export model=resnet18
+export w_bits=4
+export a_bits=8
 
 python main.py --data_path $datasets_dir --arch $model --n_bits_w $w_bits --channel_wise --n_bits_a $a_bits --act_quant --test_before_calibration
